c217fb3a3249404ad20367230b4e801fde3f6375
aaron staes <aaron.staes@gmail.com>
2021-09-01 16:56:14 +0200

    added movie poster url to form cards with stimulus

---
+++ app/javascript/controllers/movies_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  static targets = ["cards"];
+
+  connect() {
+    console.log(this.cardsTarget);
+
+    const movieArray = JSON.parse(this.element.dataset.moviesUrl);
+
+    this.element.querySelectorAll(".image-card").forEach((div) => {
+      let id = +div.querySelector("input").value;
+      console.log(id);
+      let movieHash = movieArray.find((movie) => {
+        return movie.id == id;
+      });
+      console.log(movieHash);
+      div.style.backgroundImage = `url(${movieHash.url})`;
+    });
+  }
+}
