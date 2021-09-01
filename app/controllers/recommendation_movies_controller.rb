d8e3be86ff21554a568b5ac4719297bd434378bd
Hamza El Kholy <hamza.elkhouly@mail.mcgill.ca>
2021-09-01 15:48:11 +0200

    gemfile adding dot env

--- app/controllers/recommendation_movies_controller.rb
+++ app/controllers/recommendation_movies_controller.rb
@@ -5,12 +5,26 @@ class RecommendationMoviesController < ApplicationController
 
   def create
     @recommendation_movie = RecommendationMovie.new(recommendation_movie_params)
-    raise
+    if params[:recommendation_movie][:movie_id].length > 7
+      # Call the Watchmode api on the movies
+      # Which streaming service has the most hits
+    else
+      # Prompt user to select more
+      # Generate new similar movies
+      @selected_movies = []
+      @movie_ids = params[:recommendation_movie][:movie_id]
+      @movie_ids.shift
+      # Create array of selected movies (with integers)
+      @movie_ids.map do |movie_id|
+        @selected_movies << movie_id.to_i
+      end
+
+    end
   end
 
   private
 
   def recommendation_movie_params
-    params.require(:recommendation_movie).permit(:movie_id)
+    # params.require(:recommendation_movie).permit(:movie_id)
   end
 end
