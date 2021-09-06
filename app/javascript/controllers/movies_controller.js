739cdc042c6621b94e3a85644ac297a25b7ca1c7
aaron staes <aaron.staes@gmail.com>
2021-09-06 15:11:28 +0200

    added progressbar.js to selection page

--- app/javascript/controllers/movies_controller.js
+++ app/javascript/controllers/movies_controller.js
@@ -1,12 +1,36 @@
 import { Controller } from "stimulus";
+import ProgressBar from "progressbar.js";
 
 export default class extends Controller {
-  static targets = ["cards"];
+  static targets = ["cards", "alreadyConnected"];
+
+  countMovies() {
+    let selected = document.querySelectorAll(".hide-checkbox:checked");
+    let already_selected = this.alreadyConnectedTarget.value.split(" ").length;
+    console.log(selected.length + already_selected);
+    this.accuracyBar.animate((selected.length + already_selected) / 13);
+  }
 
   connect() {
     // console.log(this.cardsTarget);
-
     const movieArray = JSON.parse(this.element.dataset.moviesUrl);
+
+    let already_selected = this.alreadyConnectedTarget.value;
+
+    this.accuracyBar = new ProgressBar.Line("#accuracy-meter", {
+      strokeWidth: 2,
+      easing: "easeInOut",
+
+      trailColor: "#fbf9f9",
+
+      from: { color: "#f7797d" },
+      to: { color: "#C6FFDD" },
+      step: function (state, line) {
+        line.path.setAttribute("stroke", state.color);
+      },
+    });
+
+    this.accuracyBar.animate(already_selected.split(" ").length / 12);
 
     this.element.querySelectorAll(".image-card").forEach((div) => {
       let id = +div.querySelector("input").value;
@@ -18,10 +42,5 @@ export default class extends Controller {
       div.style.backgroundImage = `url(${movieHash.url})`;
       div.style.backgroundSize = "cover";
     });
-  }
-
-  countMovies() {
-    let selected = document.querySelectorAll(".hide-checkbox:checked");
-    console.log(selected.length);
   }
 }
