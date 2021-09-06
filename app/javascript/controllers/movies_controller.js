4712acd0a8c6a9ebb6557047697c72af7dc4ba54
aaron staes <aaron.staes@gmail.com>
2021-09-06 11:57:25 +0200

    added stimulus action to selecting movies

--- app/javascript/controllers/movies_controller.js
+++ app/javascript/controllers/movies_controller.js
@@ -4,19 +4,24 @@ export default class extends Controller {
   static targets = ["cards"];
 
   connect() {
-    console.log(this.cardsTarget);
+    // console.log(this.cardsTarget);
 
     const movieArray = JSON.parse(this.element.dataset.moviesUrl);
 
     this.element.querySelectorAll(".image-card").forEach((div) => {
       let id = +div.querySelector("input").value;
-      console.log(id);
+      // console.log(id);
       let movieHash = movieArray.find((movie) => {
         return movie.id == id;
       });
-      console.log(movieHash);
+      // console.log(movieHash);
       div.style.backgroundImage = `url(${movieHash.url})`;
       div.style.backgroundSize = "cover";
     });
+  }
+
+  countMovies() {
+    let selected = document.querySelectorAll(".hide-checkbox:checked");
+    console.log(selected.length);
   }
 }
