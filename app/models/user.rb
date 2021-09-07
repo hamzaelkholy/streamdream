9693e41d350d5dd3f32478499928c3985324b48b
Mert Arslan <merarslan9@gmail.com>
2021-09-07 15:56:41 +0300

    fixed the backend of results

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :recommendations
   has_many :streaming_services, through: :recommendations
+  has_many :recommendation_movies, through: :recommendations
 
   validates :username, presence: true, uniqueness: true, length: { maximum: 10 }
   validates :email, uniqueness: true, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: 'invalid email'}
